9ef2ceca170897d243602d23d2b2a09f35f04d19
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-03-06 16:22:33 +0700

    Dat: Add ResponseModel for CreateOrderWithOnlinePaymentMethod

---
+++ Entity/OnlineOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class OnlineOrder
+    {
+        public int OrderId { get; set; }
+        public string Url { get; set; }
+    }
+}
